825319022a1727b187993f1ad8f5410ac68c3919
felipedemacedo <felipedemacedo.cin@gmail.com>
2016-04-07 16:46:49 -0300

    new_boq -> ni_flex

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 
 sed -i 's/{targetPlayerMajorVersion}/10.0/g' $flexlib/flex-config.xml && grep -r -l "com/images" /Licenze_Flex_3_6/src/com/* | xargs sed -i "s/com\/images/\/com\/images/g"
 
-mxmlc -load-config config/config-compiler.xml Licenze_Flex_3_6/src/new_boq.mxml -output output/new_boq.swf
+mxmlc -load-config config/config-compiler.xml Licenze_Flex_3_6/src/ni_flex.mxml -output output/ni_flex.swf
